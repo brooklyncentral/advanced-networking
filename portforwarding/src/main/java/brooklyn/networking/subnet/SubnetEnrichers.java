aec0170b3b53d76e17892b996df3bc202e9d9958
Andrew Kennedy <andrew.kennedy@cloudsoftcorp.com>
2014-12-03 17:49:33 +0100

    Fix rebind of enrichers on SubnetTier due to anonymous classes

--- portforwarding/src/main/java/brooklyn/networking/subnet/SubnetEnrichers.java
+++ portforwarding/src/main/java/brooklyn/networking/subnet/SubnetEnrichers.java
@@ -37,6 +37,7 @@ import brooklyn.util.guava.Maybe;
 import brooklyn.util.text.Strings;
 
 import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
 import com.google.common.net.HostAndPort;
 
 public class SubnetEnrichers {
@@ -44,18 +45,18 @@ public class SubnetEnrichers {
     private static final Logger log = LoggerFactory.getLogger(SubnetEnrichers.class);
 
     public static EnricherSpec<?> uriTransformingEnricher(SubnetTier subnetTier, AttributeSensor<String> original, AttributeSensor<String> target) {
-        return EnricherSpec.create(UriTransformerEnricher.class)
-                .configure(UriTransformerEnricher.SUBNET_TIER, subnetTier)
-                .configure(UriTransformerEnricher.SOURCE_SENSOR, original)
-                .configure(UriTransformerEnricher.TARGET_SENSOR, target);
+        return EnricherSpec.create(UriTransformingEnricher.class)
+                .configure(UriTransformingEnricher.SUBNET_TIER, subnetTier)
+                .configure(UriTransformingEnricher.SOURCE_SENSOR, original)
+                .configure(UriTransformingEnricher.TARGET_SENSOR, target);
     }
 
     public static EnricherSpec<?> uriTransformingEnricher(SubnetTier subnetTier, EntityAndAttribute<String> original, AttributeSensor<String> target) {
-        return EnricherSpec.create(UriTransformerEnricher.class)
-                .configure(UriTransformerEnricher.SUBNET_TIER, subnetTier)
-                .configure(UriTransformerEnricher.SOURCE_SENSOR, original.getAttribute())
-                .configure(UriTransformerEnricher.PRODUCER, original.getEntity())
-                .configure(UriTransformerEnricher.TARGET_SENSOR, target);
+        return EnricherSpec.create(UriTransformingEnricher.class)
+                .configure(UriTransformingEnricher.SUBNET_TIER, subnetTier)
+                .configure(UriTransformingEnricher.SOURCE_SENSOR, original.getAttribute())
+                .configure(UriTransformingEnricher.PRODUCER, original.getEntity())
+                .configure(UriTransformingEnricher.TARGET_SENSOR, target);
     }
 
     public static EnricherSpec<?> hostAndPortTransformingEnricher(SubnetTier subnetTier, AttributeSensor<Integer> originalPort, AttributeSensor<String> target) {
@@ -73,70 +74,92 @@ public class SubnetEnrichers {
                 .configure(HostAndPortTransformingEnricher.TARGET_SENSOR, target);
     }
 
-    public static class UriTransformerEnricher extends Transformer<Object, String> {
+    public static class UriTransformingEnricher extends Transformer<Object, String> {
+
         @SetFromFlag("subnetTier")
         public static final ConfigKey<SubnetTier> SUBNET_TIER = ConfigKeys.newConfigKey(SubnetTier.class, "enricher.uriTransformer.subnetTier");
 
         @Override
         public void init() {
-            final SubnetTier subnetTier = getConfig(SUBNET_TIER);
+            SubnetTier subnetTier = getConfig(SUBNET_TIER);
+            setConfig(TRANSFORMATION_FROM_EVENT, new UriTransformingFunction(subnetTier));
+        }
+    }
 
-            setConfig(TRANSFORMATION_FROM_EVENT, new Function<SensorEvent<Object>,String>() {
-                    @Override public String apply(SensorEvent<Object> event) {
-                        String sensorVal = Strings.toString(event.getValue());
-                        Entity source = event.getSource();
-                        Maybe<MachineLocation> machine = Machines.findUniqueMachineLocation(source.getLocations());
-                        if (sensorVal != null && machine.isPresent()) {
-                            URI uri = URI.create(sensorVal);
-                            int port = uri.getPort();
-                            if (port != -1) {
-                                HostAndPort publicTarget = subnetTier.getPortForwardManager().lookup(machine.get(), port);
-                                URI result;
-                                try {
-                                    result = new URI(uri.getScheme(), uri.getUserInfo(), publicTarget.getHostText(), publicTarget.getPort(), uri.getPath(), uri.getQuery(), uri.getFragment());
-                                } catch (URISyntaxException e) {
-                                    log.debug("Error transforming URI "+uri+", using target "+publicTarget+"; rethrowing");
-                                    throw Exceptions.propagate(e);
-                                }
-                                log.debug("sensor mapper transforming URI "+uri+" to "+result+"; target="+publicTarget);
-                                return result.toString();
-                            } else {
-                                log.debug("sensor mapper not transforming URI "+uri+" because defines no port");
-                                return sensorVal;
-                            }
-                        } else {
-                            return sensorVal;
-                        }
-                    }});
+    private static class UriTransformingFunction implements Function<SensorEvent<Object>,String> {
+
+        private final SubnetTier subnetTier;
+
+        public UriTransformingFunction(SubnetTier subnetTier) {
+            this.subnetTier = Preconditions.checkNotNull(subnetTier, "subnetTier");
+        }
+
+        @Override
+        public String apply(SensorEvent<Object> event) {
+            String sensorVal = Strings.toString(event.getValue());
+            Entity source = event.getSource();
+            Maybe<MachineLocation> machine = Machines.findUniqueMachineLocation(source.getLocations());
+            if (sensorVal != null && machine.isPresent()) {
+                URI uri = URI.create(sensorVal);
+                int port = uri.getPort();
+                if (port != -1) {
+                    HostAndPort publicTarget = subnetTier.getPortForwardManager().lookup(machine.get(), port);
+                    URI result;
+                    try {
+                        result = new URI(uri.getScheme(), uri.getUserInfo(), publicTarget.getHostText(), publicTarget.getPort(), uri.getPath(), uri.getQuery(), uri.getFragment());
+                    } catch (URISyntaxException e) {
+                        log.debug("Error transforming URI "+uri+", using target "+publicTarget+"; rethrowing");
+                        throw Exceptions.propagate(e);
+                    }
+                    log.debug("sensor mapper transforming URI "+uri+" to "+result+"; target="+publicTarget);
+                    return result.toString();
+                } else {
+                    log.debug("sensor mapper not transforming URI "+uri+" because defines no port");
+                    return sensorVal;
+                }
+            } else {
+                return sensorVal;
             }
+        }
     }
 
     public static class HostAndPortTransformingEnricher extends Transformer<Object, String> {
+
         @SetFromFlag("subnetTier")
         public static final ConfigKey<SubnetTier> SUBNET_TIER = ConfigKeys.newConfigKey(SubnetTier.class, "enricher.uriTransformer.subnetTier");
 
         public void init() {
-            final SubnetTier subnetTier = getConfig(SUBNET_TIER);
+            SubnetTier subnetTier = getConfig(SUBNET_TIER);
+            setConfig(TRANSFORMATION_FROM_EVENT, new HostAndPortTransformingFunction(subnetTier));
+        }
+    }
 
-            setConfig(TRANSFORMATION_FROM_EVENT, new Function<SensorEvent<Integer>,String>() {
-                    @Override public String apply(SensorEvent<Integer> event) {
-                        Integer sensorVal = event.getValue();
-                        Entity source = event.getSource();
-                        Maybe<MachineLocation> machine = Machines.findUniqueMachineLocation(source.getLocations());
-                        if (sensorVal != null && machine.isPresent()) {
-                            HostAndPort publicTarget = subnetTier.getPortForwardManager().lookup(machine.get(), sensorVal);
+    public static final class HostAndPortTransformingFunction implements Function<SensorEvent<Integer>,String> {
 
-                            if (publicTarget != null) {
-                                log.debug("sensor mapper transforming {} port {} to {}", new Object[] {source, sensorVal, publicTarget});
-                                return publicTarget.getHostText()+":"+publicTarget.getPort();
-                            } else {
-                                log.debug("sensor mapper not transforming {} port {}, because no port-mapping for {}", new Object[] {source, sensorVal, machine.get()});
-                                return null;
-                            }
-                        } else {
-                            return null;
-                        }
-                    }});
+        private final SubnetTier subnetTier;
+
+        public HostAndPortTransformingFunction(SubnetTier subnetTier) {
+            this.subnetTier = Preconditions.checkNotNull(subnetTier, "subnetTier");
+        }
+
+        @Override
+        public String apply(SensorEvent<Integer> event) {
+            Integer sensorVal = event.getValue();
+            Entity source = event.getSource();
+            Maybe<MachineLocation> machine = Machines.findUniqueMachineLocation(source.getLocations());
+            if (sensorVal != null && machine.isPresent()) {
+                HostAndPort publicTarget = subnetTier.getPortForwardManager().lookup(machine.get(), sensorVal);
+
+                if (publicTarget != null) {
+                    log.debug("sensor mapper transforming {} port {} to {}", new Object[] {source, sensorVal, publicTarget});
+                    return publicTarget.getHostText()+":"+publicTarget.getPort();
+                } else {
+                    log.debug("sensor mapper not transforming {} port {}, because no port-mapping for {}", new Object[] {source, sensorVal, machine.get()});
+                    return null;
+                }
+            } else {
+                return null;
+            }
         }
     }
 }
