26d07f0d429f6d4473a5c9cf0332601f5c00ab03
Aled Sage <aled.sage@gmail.com>
2015-02-06 22:19:31 +0000

    For deprecated classes, include javadoc
    Include pointer to the new code that should be used instead

--- common/src/main/java/brooklyn/networking/subnet/PortForwarderAsync.java
+++ common/src/main/java/brooklyn/networking/subnet/PortForwarderAsync.java
@@ -1,6 +1,10 @@
 package brooklyn.networking.subnet;
 
-/** Kept for persisted state backwards compatibility **/
+/** 
+ * Kept for persisted state backwards compatibility
+ * 
+ * @deprecated since 0.7.0; use {@link brooklyn.networking.common.subnet.PortForwarderAsync}
+ */
 @Deprecated
 public interface PortForwarderAsync extends brooklyn.networking.common.subnet.PortForwarderAsync {
 
--- common/src/main/java/brooklyn/networking/subnet/PortForwarderAsyncImpl.java
+++ common/src/main/java/brooklyn/networking/subnet/PortForwarderAsyncImpl.java
@@ -4,7 +4,12 @@ import brooklyn.entity.basic.EntityLocal;
 import brooklyn.location.access.PortForwardManager;
 import brooklyn.networking.common.subnet.PortForwarder;
 
-/** Kept for persisted state backwards compatibility **/
+/** 
+ * Kept for persisted state backwards compatibility
+ * 
+ * @deprecated since 0.7.0; use {@link brooklyn.networking.common.subnet.PortForwarderAsyncImpl}
+ */
+@Deprecated
 public class PortForwarderAsyncImpl extends brooklyn.networking.common.subnet.PortForwarderAsyncImpl {
 
     public PortForwarderAsyncImpl(EntityLocal adjunctEntity,
