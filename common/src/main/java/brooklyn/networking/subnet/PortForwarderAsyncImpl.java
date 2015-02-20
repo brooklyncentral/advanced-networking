063a4f1073d78de11a7b4418ca36a51ef5b44c0a
Aled Sage <aled.sage@gmail.com>
2015-02-20 19:32:22 +0000

    PortForwarderClient: backwards compatibility after names
    - The PortForwarderClient package was previously renamed,
      leaving the old named PortForwarderClient there for backwards
      compatibility reasons.
    - This adds the nested inner classes, which are required for
      being able to deserialise previously persisted state.
    - Does same for PortForwarderAsyncImpl's inner classes

--- common/src/main/java/brooklyn/networking/subnet/PortForwarderAsyncImpl.java
+++ common/src/main/java/brooklyn/networking/subnet/PortForwarderAsyncImpl.java
@@ -1,8 +1,18 @@
 package brooklyn.networking.subnet;
 
+import brooklyn.entity.Entity;
+import brooklyn.entity.basic.EntityAndAttribute;
 import brooklyn.entity.basic.EntityLocal;
+import brooklyn.location.MachineLocation;
+import brooklyn.location.PortRange;
 import brooklyn.location.access.PortForwardManager;
+import brooklyn.location.basic.Machines;
 import brooklyn.networking.common.subnet.PortForwarder;
+import brooklyn.util.net.Cidr;
+import brooklyn.util.net.Protocol;
+
+import com.google.common.base.Optional;
+import com.google.common.net.HostAndPort;
 
 /** 
  * Kept for persisted state backwards compatibility
@@ -12,9 +22,42 @@ import brooklyn.networking.common.subnet.PortForwarder;
 @Deprecated
 public class PortForwarderAsyncImpl extends brooklyn.networking.common.subnet.PortForwarderAsyncImpl {
 
+    private final PortForwarder portForwarder;
+
     public PortForwarderAsyncImpl(EntityLocal adjunctEntity,
             PortForwarder portForwarder, PortForwardManager portForwardManager) {
         super(adjunctEntity, portForwarder, portForwardManager);
+        this.portForwarder = portForwarder;
     }
 
+    private void innerClass_openFirewallPortRangeAsync(final EntityAndAttribute<String> publicIp, final PortRange portRange, final Protocol protocol, final Cidr accessingCidr) {
+        new Runnable() {
+            public void run() {
+                portForwarder.openFirewallPortRange(publicIp.getEntity(), portRange, protocol, accessingCidr);
+            }};
+    }
+
+    private void innerClass_openPortForwardingAndAdvertise(final EntityAndAttribute<Integer> privatePort, final Optional<Integer> optionalPublicPort,
+            final Protocol protocol, final Cidr accessingCidr, final EntityAndAttribute<String> whereToAdvertiseEndpoint) {
+        new Runnable() {
+            public void run() {
+                Entity entity = privatePort.getEntity();
+                Integer privatePortVal = privatePort.getValue();
+                MachineLocation machine = Machines.findUniqueMachineLocation(entity.getLocations()).get();
+                HostAndPort publicEndpoint = portForwarder.openPortForwarding(machine, privatePortVal, optionalPublicPort, protocol, accessingCidr);
+                
+                // TODO What publicIpId to use in portForwardManager.associate? Elsewhere, uses jcloudsMachine.getJcloudsId().
+                portForwarder.getPortForwardManager().associate(machine.getId(), publicEndpoint, machine, privatePortVal);
+                whereToAdvertiseEndpoint.setValue(publicEndpoint.getHostText()+":"+publicEndpoint.getPort());
+            }};
+    }
+
+    /**
+     * @deprecated since 0.7.0; use {@link brooklyn.networking.common.subnet.PortForwarderAsyncImpl.DeferredExecutor}
+     */
+    protected class DeferredExecutor<T> extends brooklyn.networking.common.subnet.PortForwarderAsyncImpl.DeferredExecutor<T> {
+        public DeferredExecutor(String description, EntityAndAttribute<T> attribute, Runnable task) {
+            super(description, attribute, task);
+        }
+    }
 }
--- common/src/main/java/brooklyn/networking/subnet/PortForwarderClient.java
+++ common/src/main/java/brooklyn/networking/subnet/PortForwarderClient.java
@@ -1,6 +1,10 @@
 package brooklyn.networking.subnet;
 
+import brooklyn.config.ConfigKey;
+import brooklyn.entity.Entity;
+import brooklyn.event.AttributeSensor;
 import brooklyn.networking.common.subnet.PortForwarder;
+import brooklyn.util.exceptions.Exceptions;
 
 import com.google.common.base.Supplier;
 
@@ -13,5 +17,47 @@ import com.google.common.base.Supplier;
 public class PortForwarderClient extends brooklyn.networking.common.subnet.PortForwarderClient {
     public PortForwarderClient(Supplier<PortForwarder> supplier) {
         super(supplier);
+    }
+    
+    private static PortForwarder innerClass_fromMethodOnEntity(final Entity entity, final String getterMethodOnEntity) {
+        return new PortForwarderClient(new Supplier<PortForwarder>() {
+            @Override
+            public PortForwarder get() {
+                PortForwarder result;
+                try {
+                    result = (PortForwarder) entity.getClass().getMethod(getterMethodOnEntity).invoke(entity);
+                } catch (Exception e) {
+                    Exceptions.propagateIfFatal(e);
+                    throw new IllegalStateException("Cannot invoke "+getterMethodOnEntity+" on "+entity+" ("+entity.getClass()+"): "+e, e);
+                }
+                if (result==null)
+                    throw new IllegalStateException("No PortForwarder available via "+getterMethodOnEntity+" on "+entity+" (returned null)");
+                return result;
+            }
+        });
+    }
+    
+    private static PortForwarder innerClass_fromConfigOnEntity(final Entity entity, final ConfigKey<PortForwarder> configOnEntity) {
+        return new PortForwarderClient(new Supplier<PortForwarder>() {
+            @Override
+            public PortForwarder get() {
+                PortForwarder result = (PortForwarder) entity.getConfig(configOnEntity);
+                if (result==null)
+                    throw new IllegalStateException("No PortForwarder available via "+configOnEntity+" on "+entity+" (returned null)");
+                return result;
+            }
+        });
+    }
+    
+    private static PortForwarder innerClass_fromAttributeOnEntity(final Entity entity, final AttributeSensor<PortForwarder> attributeOnEntity) {
+        return new PortForwarderClient(new Supplier<PortForwarder>() {
+            @Override
+            public PortForwarder get() {
+                PortForwarder result = (PortForwarder) entity.getAttribute(attributeOnEntity);
+                if (result==null)
+                    throw new IllegalStateException("No PortForwarder available via "+attributeOnEntity+" on "+entity+" (returned null)");
+                return result;
+            }
+        });
     }
 }
